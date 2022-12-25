5567e8873d8b954732f330a037e2f067afed2b05
Emre Buruk <emre.buruk007@gmail.com>
2022-12-26 00:29:50 +0200

    Fixed some bugs on loading the image to a buffer.

--- src/LoadPNG.c
+++ src/LoadPNG.c
@@ -5,32 +5,41 @@
 
 
 int readPng() {
-    char *buffer = malloc(MAX_SIZE);  // input buffer for image in chars
+    unsigned char *buffer = malloc(MAX_SIZE);  // input buffer for image in chars
 
+    // memory allocation error checking
     if (!buffer) {
         fprintf(stderr, "Couldn't allocate memory\n");
         return 1;
     }
 
-    FILE *f = fopen("../testImage.png", "r+"); // opening file
+    FILE *f = fopen("../testImage.png", "r"); // opening file
 
-    if (!f) {   // checking and reporting possible file open errors
+    // checking and reporting possible file open errors
+    if (!f) {
         perror("fopen");
         free(buffer);
         return 1;
     }
-    printf("%x\n", buffer[0]);
-    checkHeader(buffer);
+
 
     // taking the header, saving it to buffer
     unsigned long size = fread(buffer, 1, MAX_SIZE, f);
 
-//    for (int i = 0; i < 8; i++) {
-//        printf("%x\n", buffer[i]);
-//    }
+    // checking to see if the file is a PNG
+    checkHeader(buffer);
 
-    printf("%lu\n", size);
-    printf("%s\n", buffer);
+    // below are debug codes
+//    for (int i = 0; i < 8; i++) {
+//        printf("%02X ", buffer[i]);
+//    }
+//    printf("\n");
+//    for (int i = 0; i < 8; i++) {
+//        printf("%c ", buffer[i]);
+//    }
+//
+//    printf("\n%lu\n", size);
+//    printf("%s\n", buffer);
 
     fclose(f);
     free(buffer);
@@ -41,12 +50,13 @@ int readPng() {
 void validate(int val, const char *msg) {
     if(!val) {
         fprintf(stderr, "Invalid file: %s\n", msg);
+        perror("");
         exit(1);
     }
 }
 
 void checkHeader(const char *buf) {
-    validate(buf[0] == '\x89', "header byte 1");
+    validate((unsigned char)buf[0] == 0x89, "header byte 1");
     validate((unsigned char)buf[1] == 0x50, "header byte 2");               //letter P
     validate((unsigned char)buf[2] == 0x4E, "header byte 3");               //letter N
     validate((unsigned char)buf[3] == 0x47, "header byte 4");               //letter G

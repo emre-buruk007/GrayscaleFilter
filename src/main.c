eb6210b27f2fff825a7c11ab21dba31d9a6314d5
Emre Buruk <emre.buruk007@gmail.com>
2023-01-04 16:44:50 +0200

    After a week of research I decided that LodePNG is a better option for the task when compared with libpng. Reason being is that libpng is much more difficult than I could handle.
    Loaded the LodePNG library and experimented a little, still not working.

--- src/main.c
+++ src/main.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "lodepng.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
+    // Load the PNG image
+    unsigned error;
+    unsigned char *image;
+    unsigned width, height;
+    error = lodepng_decode32_file(&image, &width, &height, "inputImage/testImage.png");
+    if (error) {
+        printf("Error %u: %s\n", error, lodepng_error_text(error));
+        return 1;
+    }
 
+    // Convert the image to grayscale
+    for (unsigned y = 0; y < height; y++) {
+        for (unsigned x = 0; x < width; x++) {
+            unsigned char *pixel = image + (y * width + x) * 4;
+            unsigned char gray = (pixel[0] + pixel[1] + pixel[2]) / 3;
+            pixel[0] = gray;
+            pixel[1] = gray;
+            pixel[2] = gray;
+        }
+    }
 
+    // Save the grayscale image
+    error = lodepng_encode32_file("outputImage/output.png", image, width, height);
+    if (error) {
+        printf("Error %u: %s\n", error, lodepng_error_text(error));
+        return 1;
+    }
 
-
-
+    // Clean up
+    free(image);
     return 0;
 }
-
-

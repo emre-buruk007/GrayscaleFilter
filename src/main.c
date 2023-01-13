8ada413dce2910889da2d353e6be0f2e087d00f4
Emre Buruk <emre.buruk007@gmail.com>
2023-01-13 17:12:11 +0200

    Nothing significant achieved. After lots of research I believe I improved it a bit, however it still does not work and is buggy.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+int main(void) {
     // Open the input file
     FILE *fp = fopen("../inputImage/testImage.png", "rb");
     if (!fp) {
@@ -9,37 +9,44 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // Read the file into a buffer
-    fseek(fp, 0, SEEK_END);
-    size_t size = ftell(fp);
-    rewind(fp);
-    unsigned char *buffer = (unsigned char*)malloc(size);
-    fread(buffer, 1, size, fp);
+    // Read the PNG header to get the width and height
+    unsigned width, height;
+    unsigned char header[8];   // PNG header 8 bytes
+    unsigned char IHDR[25];    // IHDR chunk 25 bytes
+    fread(header, 1, 8, fp);
+
+    // PNG header standard
+    if (header[0] == 137 && header[1] == 80 && header[2] == 78 && header[3] == 71 && header[4] == 13 && header[5] == 10 && header[6] == 26 && header[7] == 10) {
+        fread(IHDR, 1, 8, fp);
+        while(IHDR[4] != 'I' && IHDR[5] != 'H' && IHDR[6] != 'D' && IHDR[7] != 'R')
+        {
+            fread(IHDR, 1, 8, fp);
+        }
+
+        // bad calculaton here
+        width = (IHDR[4] << 24) | (IHDR[5] << 16) | (IHDR[6] << 8) | IHDR[7];
+        height = (IHDR[8] << 24) | (IHDR[9] << 16) | (IHDR[10] << 8) | IHDR[11];
+    } else {
+        printf("Error: Input file is not a valid PNG image\n");
+        return 1;
+    }
+    size_t size = width*height*3;
+    unsigned char *image_data = malloc(size);
+    fread(image_data, 1, size, fp);
 
     // Close the input file
     fclose(fp);
 
-    // Check that the file is a PNG image
-    if (size < 8 || buffer[0] != 137 || buffer[1] != 80 || buffer[2] != 78 || buffer[3] != 71 || buffer[4] != 13 || buffer[5] != 10 || buffer[6] != 26 || buffer[7] != 10) {
-        printf("Error: Input file is not a valid PNG image\n");
+    // Check the size of the image data in the buffer
+    if(size == 0)
+    {
+        printf("Error: Image data is empty or corrupted\n");
         return 1;
     }
-
-    // Parse the PNG header to get the width and height
-    unsigned width, height;
-    if (buffer[12] == 'I' && buffer[13] == 'H' && buffer[14] == 'D' && buffer[15] == 'R') {
-        // the width and height of the image are stored as 32-bit unsigned integers, so we do bitshifting to get the int number
-        width = (buffer[16] << 24) | (buffer[17] << 16) | (buffer[18] << 8) | buffer[19];
-        height = (buffer[20] << 24) | (buffer[21] << 16) | (buffer[22] << 8) | buffer[23];
-    } else {
-        printf("Error: Unable to parse PNG header\n");
-        return 1;
-    }
-
     // Convert the image to grayscale
     for (unsigned y = 0; y < height; y++) {
         for (unsigned x = 0; x < width; x++) {
-            unsigned char *pixel = buffer + (y * width + x) * 4;
+            unsigned char *pixel = image_data + (y * width + x) * 3;
             unsigned char gray = (pixel[0] + pixel[1] + pixel[2]) / 3;
             pixel[0] = gray;
             pixel[1] = gray;
@@ -55,13 +62,26 @@ int main(int argc, char *argv[]) {
     }
 
     // Write the image data to the output file
-    fwrite(buffer, 1, size, fp);
+    if(fwrite(image_data, 1, size, fp) != size)
+    {
+        printf("Error: Unable to write image data to output file\n");
+        return 1;
+    }
 
     // Close the output file
     fclose(fp);
 
+    // Check for errors
+    if(ferror(fp))
+    {
+        printf("Error: An error occurred during the file operation\n");
+        return 1;
+    }
+
     // Clean up
-    free(buffer);
+    free(image_data);
     return 0;
 }
+
+
 
